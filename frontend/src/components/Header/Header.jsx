5377b1c97fbb68735fc177061930918f8a9f88b1
whaleeeyyyyy <158804943+whaleeeyyyyy@users.noreply.github.com>
2025-07-17 09:58:57 +0800

    Update header (pls check if okay or not)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Header.css";
+import assets from "../../assets/assets";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
         <div className="header-left">
-          <div className="logo">LOGO</div>
+          <div className="logo">
+            <img src={assets.SRC_Black} alt="Logo" />
+          </div>
           <button
-            className="homepage-button"
+            className="sabah-road-care-button" // Changed from homepage-button to sabah-road-care-button
             onClick={() => (window.location.href = "/")}
             onMouseEnter={(e) => (e.target.style.backgroundColor = "#c4c9d0")}
             onMouseLeave={(e) => (e.target.style.backgroundColor = "#d1d5db")}
